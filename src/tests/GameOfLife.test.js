bace8238d78f3902ad9236c974b6e496c4b9cc88
Gokul-Ga <129526418+Gokul-Ga@users.noreply.github.com>
2023-11-21 20:34:09 +0530

    Update GameOfLife.test.js

--- src/tests/GameOfLife.test.js
+++ src/tests/GameOfLife.test.js
@@ -1,4 +1,3 @@
-// Import necessary dependencies for testing
 import React from 'react';
 import { render, fireEvent, screen, waitFor } from '@testing-library/react';
 import GameOfLife from '../components/GameOfLife';
